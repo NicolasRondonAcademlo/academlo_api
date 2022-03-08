b7d08bcd03f98cc81601687be63369ae0b3eb735
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-07 20:59:37 -0500

    part1

---
+++ app.py
@@ -0,0 +1,19 @@
+
+# def app(environ, start_response):
+#     response_body = b"Hello World"
+#     status = "200 ok"
+#     start_response(status, headers=[])
+#     return iter([response_body])
+
+from api import API
+app = API()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from HOME page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from ABOUT page"
+
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+Cliente 
+Servidor
+PythonApp
+
+Cliente -> Servidor -> Midleware ->
+PythonApp -> Midleware -> 
+Servidor
+-> Cliente
+
+"Hola Aplicacion despirta y haz algo con esta peticion"
+
+WSGI
